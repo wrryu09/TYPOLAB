5ce26ebf632678d2e2fbc7790aab4f35e2bc190c
wrryu09 <rryu09@ewhain.net>
2023-10-01 01:08:19 +0900

    🔧 tailwindcss, svgr

--- typolab/next.config.js
+++ typolab/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    disableStaticImages: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.export = nextConfig;
---
+++ typolab/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      black: "#1d1e1d",
+      white: "#f5f5f5",
+      fog: "#eff4f5",
+      lightGrey: "#c0c6c4",
+      greenGrey: "#708783",
+      darkGreen: "#132127",
+      blueblue: "#4f89a7",
+      red: "#ec5424",
+      yellow: "#ffa621",
+    },
+    fontFamily: {},
+    screens: {
+      mobile: { max: "430px" },
+      tablet: { min: "431px", max: "1023px" },
+      desktop: { min: "1024px" },
+    },
+  },
+
+  plugins: [],
+};
