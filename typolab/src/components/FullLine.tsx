af1cc87033c169c26e2ce13246a7e5eb6f053ff0
wrryu09 <rryu09@ewhain.net>
2023-10-01 18:00:02 +0900

    💄 change FullLine width

--- typolab/src/components/FullLine.tsx
+++ typolab/src/components/FullLine.tsx
@@ -4,11 +4,11 @@ type LineColor = {
   color: "white" | "yellow" | "black" | "greenGrey" | "darkGreen";
 };
 const lineColors = {
-  white: "h-px w-full bg-white",
-  yellow: "h-px w-full bg-yellow",
-  black: "h-px w-full bg-black",
-  greenGrey: "h-px w-full bg-greenGrey",
-  darkGreen: "h-px w-full bg-darkGreen",
+  white: "h-px w-11/12 bg-white",
+  yellow: "h-px w-11/12 bg-yellow",
+  black: "h-px w-11/12 bg-black",
+  greenGrey: "h-px w-11/12 bg-greenGrey",
+  darkGreen: "h-px w-11/12 bg-darkGreen",
 };
 const FullLine = ({ color }: LineColor): JSX.Element => {
   return <div className={lineColors[color]}></div>;
