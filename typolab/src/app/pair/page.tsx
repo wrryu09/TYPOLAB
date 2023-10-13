380412bea2038dd990be69120b109cae31e978e8
wrryu09 <rryu09@ewhain.net>
2023-10-13 22:17:40 +0900

    🚧 pair page publishing

--- typolab/src/app/pair/page.tsx
+++ typolab/src/app/pair/page.tsx
@@ -1,11 +1,160 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import React, { useState } from "react";
+import SizedBox from "@/components/SizedBox";
+import BackArrow from "@/components/BackArrow";
+import Footer from "@/components/Footer";
+import { HatIco, PlusIco } from "../../../public/svgs";
+import FontCard from "@/components/FontCard";
+
+type Props = {};
 
 const Pair = (props: Props) => {
+  const subTitleStyle = "font-Bayon text-6xl pb-8";
+  type Tag = {
+    id: number;
+    name: string;
+    selected: boolean;
+  };
+  const [tagList, setTagList] = useState<Tag[]>([
+    { id: 1, name: "#이화여자대학교", selected: true },
+    { id: 2, name: "#도전학기제", selected: true },
+    { id: 3, name: "#16기", selected: false },
+    { id: 4, name: "#타이포랩", selected: true },
+    { id: 5, name: "#발표자료에좋은", selected: true },
+    { id: 6, name: "#코딩하기좋은", selected: true },
+  ]);
+  function handleTagSelection(tagId: number) {
+    const tagArr = [...tagList];
+    tagList.forEach((tag) => {
+      if (tag.id === tagId) {
+        tag.selected = !tag.selected;
+      }
+    });
+    setTagList(tagArr);
+  }
   return (
-    <div>Pair</div>
-  )
-}
+    <div className="bg-fog h-full text-darkGreen flex flex-col items-center">
+      {/* <link
+        rel="stylesheet"
+        href={`https://fonts.googleapis.com/css2?family=${fontFamily}`}
+      />
+      <style>
+        {`.fontFamily{
+    font-family: ${fontFamily};
+  }
+  .fontWeight{
+    font-weight: ${varient}
+  }
+  }`}
+      </style> */}
+      <BackArrow />
+      <HatIco width={"25%"} className="rotate-180 self-center top-0 absolute" />
+      <SizedBox height={10} />
 
-export default Pair
+      <div className="w-10/12 flex flex-col text-center items-center justify-center">
+        {/* tag section */}
+        <h1 className={subTitleStyle}>TAG</h1>
+        <div className="flex flex-wrap gap-2 mb-40">
+          {tagList.map((tag) => {
+            return (
+              <h1
+                key={tag.id + tag.name}
+                className={`px-3 py-1 border border-lightGrey rounded-md flex shrink-0 justify-center hover:bg-red ${
+                  tag.selected ? "bg-red font-semibold" : "bg-fog"
+                }`}
+                onClick={() => {
+                  handleTagSelection(tag.id);
+                }}
+              >
+                {tag.name}
+              </h1>
+            );
+          })}
+        </div>
+
+        {/* font section */}
+        <div className="flex self-start gap-32 mb-16">
+          {/* 1st set */}
+          <div className="flex flex-col items-start">
+            <h1 className="text-4xl">Black Ops One</h1>
+            <p>San-Serif, display</p>
+          </div>
+          {/* 2nd set */}
+          <div className="flex flex-col items-start">
+            <h1 className="text-4xl">Noto Sans</h1>
+            <p>San-Serif, display</p>
+          </div>
+        </div>
+
+        {/* font display box */}
+        <div className="w-full flex flex-col items-start gap-y-2 border border-greenGrey px-8 py-[34px] rounded-lg mb-60">
+          <h1 className="text-4xl">Black Ops One</h1>
+          <p>
+            happy happyhappy happyhappy happyhappy happyhappy happyhappy
+            happyhappy happy
+          </p>
+        </div>
+
+        {/* box section */}
+        <div className="w-full mb-60">
+          <h1 className={subTitleStyle}>ADD TO YOUR BOX</h1>
+          <div className="flex flex-col items-center gap-y-6">
+            {/* 1st set */}
+            <div className="flex w-5/12 justify-between">
+              <div className="flex flex-col items-start">
+                <h1 className="text-4xl">Noto Sans</h1>
+                <p>San-Serif, display</p>
+              </div>
+              <PlusIco className="w-8" />
+            </div>
+            {/* 2nd set */}
+            <div className="flex w-5/12 justify-between">
+              <div className="flex flex-col items-start">
+                <h1 className="text-4xl">Noto Sans</h1>
+                <p>San-Serif, display</p>
+              </div>
+              <PlusIco className="w-8" />
+            </div>
+          </div>
+        </div>
+
+        {/* font info section */}
+        <div className="mb-96">
+          <h1 className={subTitleStyle}>FONT INFO</h1>
+          {/* <FontCard idx={0} />
+        <FontCard idx={1}/> */}
+          <HatIco className="absolute w-full left-0 right-0" />
+        </div>
+
+        {/* user guide section */}
+        <div className="mb-40">
+          <h1 className={subTitleStyle}>HOW TO USE</h1>
+          <div className="flex flex-wrap gap-2 mb-10">
+            {/* 해당되는 태그만 보이기 */}
+            {tagList.map((tag) => {
+              return (
+                <h1
+                  key={tag.id + tag.name + "selected"}
+                  className={`px-3 py-1 border border-lightGrey rounded-md flex shrink-0 justify-center
+                  bg-fog
+                `}
+                >
+                  {tag.name}
+                </h1>
+              );
+            })}
+          </div>
+          <p>
+            happy happyhappy happyhappy happyhappy happyhappy happyhappy
+            happyhappy happy
+          </p>
+        </div>
+      </div>
+      <SizedBox height={20} />
+      <Footer />
+    </div>
+  );
+};
+
+export default Pair;
