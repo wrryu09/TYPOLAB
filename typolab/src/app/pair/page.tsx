75f379d12a0a49e3ec2b067133d97c526890d1e4
wrryu09 <rryu09@ewhain.net>
2023-10-16 14:57:13 +0900

    🎨 detach putFontSetToBox func #6

--- typolab/src/app/pair/page.tsx
+++ typolab/src/app/pair/page.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 import { HatIco, PlusIco } from "../../../public/svgs";
 import FontCard from "@/components/FontCard";
 import FontSetting from "@/containers/search/FontSetting";
-import { FontSet, FontSetArr } from "@/types/types";
+import { putFontSetToBox } from "@/services/putFontSetToBox";
 
 type Props = {};
 
@@ -40,32 +40,6 @@ const Pair = (props: Props) => {
       }
     });
     setTagList(tagArr);
-  }
-
-  function putFontSetToBox(fontSet: FontSet) {
-    const currentBox = localStorage.getItem("box");
-    if (currentBox && currentBox !== "null" && currentBox !== "undefined") {
-      const addData: FontSetArr = JSON.parse(currentBox);
-      if (
-        addData.find((ele) => {
-          if (
-            ele.family === fontSet.family &&
-            ele.weight === fontSet.weight &&
-            ele.size === fontSet.size
-          ) {
-            return true;
-          }
-        })
-      ) {
-        console.log("already have same set");
-      } else {
-        addData.push(fontSet);
-        localStorage.setItem("box", JSON.stringify(addData));
-      }
-    } else {
-      localStorage.setItem("box", JSON.stringify([fontSet]));
-    }
-    console.log("boxSet :", localStorage.getItem("box"));
   }
 
   return (
