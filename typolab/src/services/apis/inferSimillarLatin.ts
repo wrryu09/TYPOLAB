24ed8cc4e83cdf0ebd393d34f1914f57fd5f3e4a
wrryu09 <rryu09@ewhain.net>
2023-11-21 03:54:23 +0900

    ⚡️ send fontname & var to infer

--- typolab/src/services/apis/inferSimillarLatin.ts
+++ typolab/src/services/apis/inferSimillarLatin.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 
 export function inferSimillarLatin(font: FontNameNVar) {
   return axios
-    .get(`${process.env.NEXT_PUBLIC_SERVER_URL}infer/simillarLatin`)
+    .get(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}infer/simillarLatin/${font.name}_${font.variants}`
+    )
     .then((res) => {
       return res.data.data.matches;
     })
