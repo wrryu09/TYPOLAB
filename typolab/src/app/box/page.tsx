f4132b5596e6089a2b79ff821e192dacd479be57
wrryu09 <rryu09@ewhain.net>
2023-12-04 04:42:02 +0900

    💄 Box page mobile view

--- typolab/src/app/box/page.tsx
+++ typolab/src/app/box/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import SizedBox from "@/components/SizedBox";
 import BackArrow from "@/components/BackArrow";
 import Footer from "@/components/Footer";
-import { DotLine, HatIco, PlusIco } from "../../../public/svgs";
+import { DotLine, HatIco } from "../../../public/svgs";
 import BoxCard from "@/containers/box/BoxCard";
 import { FontSet, FontSetArr } from "@/types/types";
 import { useRouter } from "next/navigation";
@@ -14,7 +13,7 @@ type Props = {};
 const Box = (props: Props) => {
   const router = useRouter();
   const [boxContent, setBoxContent] = useState<FontSetArr>();
-  const subTitleStyle = "font-Bayon text-6xl pb-8";
+  const subTitleStyle = "mobile:text-4xl mobile:pb-4 font-Bayon text-6xl pb-8";
 
   function removeItemFromBox(font: FontSet) {
     const storedArr = localStorage.getItem("box");
@@ -49,10 +48,9 @@ const Box = (props: Props) => {
     <div className="bg-blueblue h-full text-darkGreen flex flex-col items-center">
       <BackArrow />
       <HatIco width={"25%"} className="rotate-180 self-center top-0 absolute" />
-      <SizedBox height={10} />
-      <div className="w-10/12 flex flex-col text-center items-center justify-center">
+      <div className="mobile:mt-[4rem] mobile:mb-[20rem] w-10/12 flex flex-col text-center items-center justify-center mt-[10rem] mb-[30rem]">
         <h1 className={subTitleStyle}>MY BOX</h1>
-        <div className="w-full items-center flex flex-col gap-y-8 mb-40">
+        <div className="mobile:mb-20 w-full items-center flex flex-col gap-y-8 mb-40">
           {boxContent
             ? boxContent.map((ele, idx) => {
                 return (
@@ -88,9 +86,7 @@ const Box = (props: Props) => {
           </button>
         </div>
       </div>
-      <SizedBox height={30} />
       <Footer />
-      <SizedBox height={20} />
     </div>
   );
 };
