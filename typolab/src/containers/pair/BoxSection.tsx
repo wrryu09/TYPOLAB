b2cba7fcfe73fbfb34089b607084e8c4defb6e38
wrryu09 <rryu09@ewhain.net>
2024-01-01 23:11:17 +0900

    🎨 pair 플로팅버튼 수정

--- typolab/src/containers/pair/BoxSection.tsx
+++ typolab/src/containers/pair/BoxSection.tsx
@@ -20,8 +20,10 @@ const BoxSection = (props: Props) => {
     <>
       <div className="mb-8">
         <h1 className={`${props.subTitleStyle} mb-2`}>ADD TO YOUR BOX</h1>
-        <p>+ 버튼을 눌러 박스에 선택한 글꼴 정보를 저장해 보세요!</p>
-        <p>BOX 페이지에서 확인할 수 있습니다.</p>
+        <div className="break-keep text-sm">
+          <p>+ 버튼을 눌러 박스에 선택한 글꼴 정보를 저장해 보세요!</p>
+          <p>BOX 페이지에서 확인할 수 있습니다.</p>
+        </div>
       </div>
       <div className="mobile:mb-4 mb-8 mobile:py-8 w-full py-20 border-2 border-lightGrey rounded-3xl shadow-xl">
         <div className="flex flex-col items-center gap-y-6">
