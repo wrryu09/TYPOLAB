ad07eee8738456b2e2a6ada2c6463430f369a1c1
wrryu09 <rryu09@ewhain.net>
2024-09-03 14:34:11 +0900

    feat: 저장 토글기능

--- typolab/src/containers/pair/BoxSet.tsx
+++ typolab/src/containers/pair/BoxSet.tsx
@@ -1,50 +1,70 @@
-import React from "react";
+import { useState } from "react";
 import { CheckIco, PlusIco } from "../../../public/svgs";
 import putFontSetToBox from "@/services/putFontSetToBox";
 import { FontNameVarSet } from "@/types/types";
+import removeFontSetFromBox from "@/services/removeFontSetFromBox";
+import isFontSetInBox from "@/services/isFontSetInBox";
 
-type Props = {
+type BoxSetProps = {
   font: FontNameVarSet;
   displaySize: number;
-  isInBox: boolean;
-  setItInBox: React.Dispatch<React.SetStateAction<boolean>>;
   boxNum: 1 | 2;
 };
 
-const BoxSet = (props: Props) => {
+const BoxSet = ({ font, displaySize, boxNum }: BoxSetProps) => {
+  const [fontInBox, setFontInBox] = useState(
+    isFontSetInBox({ family: font.name, size: 32, weight: font.variants })
+  );
+
+  const isInBox = () => {
+    setFontInBox(true);
+  };
+  const isNotInBox = () => {
+    setFontInBox(false);
+  };
+
+  const handleClick = () => {
+    if (fontInBox) {
+      removeFontSetFromBox({
+        family: font.name,
+        size: 32,
+        weight: font.variants,
+      });
+      isNotInBox();
+    } else {
+      putFontSetToBox(
+        {
+          family: font.name,
+          weight: font.variants,
+          size: displaySize,
+        },
+        isInBox
+      );
+    }
+  };
   return (
     <div className="mobile:w-9/12 w-5/12">
-      {props.font.name !== "none" ? (
+      {font.name && (
         <div className="flex justify-between">
           <div className="flex flex-col items-start text-start">
             <h1
-              className={`mobile:text-2xl text-4xl fontFamily${props.boxNum}FontFam`}
+              className={`mobile:text-2xl text-4xl fontFamily${boxNum}FontFam`}
             >
-              {props.font.name}
+              {font.name}
             </h1>
             <p className="mobile:text-sm">
-              {props.font.variants}, {props.displaySize}pt
+              {font.variants}, {displaySize}pt
             </p>
           </div>
-          {props.isInBox ? (
-            <CheckIco className={"mobile:w-6 shrink-0 fill-red w-8"} />
-          ) : (
-            <PlusIco
-              className="mobile:w-6 shrink-0 w-8"
-              onClick={() => {
-                putFontSetToBox(
-                  {
-                    family: props.font.name,
-                    weight: props.font.variants,
-                    size: props.displaySize,
-                  },
-                  props.setItInBox
-                );
-              }}
-            />
-          )}
+          <div onClick={handleClick}>
+            {fontInBox ? (
+              <CheckIco className={"mobile:w-6 shrink-0 fill-red w-8"} />
+            ) : (
+              <PlusIco className="mobile:w-6 shrink-0 w-8" />
+            )}
+          </div>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
