53d8490b8dfcf08263674912e7c8b0acc757f88a
wrryu09 <rryu09@ewhain.net>
2024-09-15 23:29:22 +0900

    chore: set smp config

--- typolab/next.config.js
+++ typolab/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import("next").NextConfig} */
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       use: ["@svgr/webpack"],
     });
+    config.plugins.push(smp);
     return config;
   },
 };
